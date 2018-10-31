14aec3bad75c55e751ef13eacc7111b7a966bfc7
a.kalita <ext.a.kalita@intrum.com>
2018-10-31 09:14:26 +0200

    [master] Changing ip resolver provider

--- src/main/java/lv/askfm/integration/CountryResponse.java
+++ src/main/java/lv/askfm/integration/CountryResponse.java
@@ -1,17 +1,17 @@
 package lv.askfm.integration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 public class CountryResponse {
 
-
-  @JsonProperty(value = "country_code")
   private String countryCode;
 
-  @JsonProperty(value = "country_name")
+  @JsonProperty(value = "country")
   private String countryName;
+
+  private String city;
 }
